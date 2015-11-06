5b363c6c58c55a75ff2532f60fc97d989573ac04
usaruner <usaruner@gmail.com>
2015-11-06 11:23:02 -0800

    Update Cow.java

--- Cow.java
+++ Cow.java
@@ -1,4 +1,16 @@
 class Cow implements Animal 
-{     
-     //your code here
+{        
+  protected String mySound;        
+  protected String myType;
+
+  public Cow (String type, String sound){
+   myType = type;         
+  mySound = sound;     
+     }     
+     public Cow()     {         
+         myType = "unknown";         
+         mySound = "unknown";     
+     }      
+     public String getSound(){return mySound;}     
+     public String getType(){return myType;}
 }
