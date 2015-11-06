3cdaaa7f8ec9638df7c320bdca4d2dda3f31daf3
usaruner <usaruner@gmail.com>
2015-11-06 11:17:55 -0800

    Update Chick.java

--- Chick.java
+++ Chick.java
@@ -1,4 +1,14 @@
 class Chick implements Animal 
-{     
-     //your code here
+{        
+  private String mySound;        
+  private String myType;
+
+  public Chick (String type, String sound){
+  myType = type;
+  mySound = sound;
+  } 
+  public String getSound(){return mySound;}
+  public String getType(){return myType;}
 }
+
+
