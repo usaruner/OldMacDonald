a0891023441890e5e97a91dc7bc5aeaef481cbe4
usaruner <usaruner@gmail.com>
2015-11-06 11:23:57 -0800

    Update Farm.java

--- Farm.java
+++ Farm.java
@@ -1,4 +1,15 @@
 class Farm 
 {     
-   //your code here
-}
+   private Animal[] alotOfAnimals = new Animal[3];
+   public Farm() {
+   alotOfAnimals[0] = new NamedCow("cow","jessie","moo");
+   alotOfAnimals[1] = new Chick("chick","cluck");
+   alotOfAnimals[2] = new Pig("pig","oink");
+   }
+   public void AnimalSounds(){
+     for(int i = 0 ; i < alotOfAnimals.length ; i++){
+      System.out.println( alotOfAnimals[i].getType() + " goes " + alotOfAnimals[i].getSound() );       }       
+      System.out.println( "The cow is known as " + ((NamedCow)alotOfAnimals[0]).getName() );    
+       
+     }
+   }
